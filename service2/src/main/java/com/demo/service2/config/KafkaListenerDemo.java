d9413d0116d7b78fef757ecc636da5cf5cb94a98
Amit Malik <amimalik2@publicisgroupe.net>
2021-08-11 20:59:35 +0530

    Some Kafka Configuration added.

--- service2/src/main/java/com/demo/service2/config/KafkaListenerDemo.java
+++ service2/src/main/java/com/demo/service2/config/KafkaListenerDemo.java
@@ -1,13 +1,15 @@
 package com.demo.service2.config;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KafkaListenerDemo {
 
 	//	@Autowired
 	//	private KafkaTemplate<String, String> kafkaTemplate;
 
-	@KafkaListener(topics = "testDemo")
+	@KafkaListener(topics = "testDemo", groupId = "mygroup")
 	public void listen(String message) {
 		System.out.println("Received Messasge in group " + message);
 	}
